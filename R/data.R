9ec17959a99e1db973574ea3d093937b18252edf
Jesús Lagos <47817326+Jelagmil@users.noreply.github.com>
2019-09-10 15:50:54 +0200

    Add files via upload

---
+++ R/data.R
@@ -0,0 +1,72 @@
+#' 3 variables for Spain
+#'
+#' Se ha calculado el ratio de Goles y xG a favor y en contra para los equipos espanoles
+#'
+#' @format A data frame with 20 rows and 3 variables:
+#' \itemize{
+#'   \item Equipo: Team ID (character)
+#'   \item Favor: Goals/xG Goals Favor (double)
+#'   \item Contra: Goals/xG Goals Against (double)
+#' }
+"Spain"
+
+##' 3 variables for France
+#'
+#' Se ha calculado el ratio de Goles y xG a favor y en contra para los equipos espanoles
+#'
+#' @format A data frame with 20 rows and 3 variables:
+#' \itemize{
+#'   \item Equipo: Team ID (character)
+#'   \item Favor: Goals/xG Goals Favor (double)
+#'   \item Contra: Goals/xG Goals Against (double)
+#' }
+"France"
+
+###' 3 variables for England
+#'
+#' Se ha calculado el ratio de Goles y xG a favor y en contra para los equipos espanoles
+#'
+#' @format A data frame with 20 rows and 3 variables:
+#' \itemize{
+#'   \item Equipo: Team ID (character)
+#'   \item Favor: Goals/xG Goals Favor (double)
+#'   \item Contra: Goals/xG Goals Against (double)
+#' }
+"England"
+
+
+####' 3 variables for Germany
+#'
+#' Se ha calculado el ratio de Goles y xG a favor y en contra para los equipos espa?noles
+#'
+#' @format A data frame with 20 rows and 3 variables:
+#' \itemize{
+#'   \item Equipo: Team ID (character)
+#'   \item Favor: Goals/xG Goals Favor (double)
+#'   \item Contra: Goals/xG Goals Against (double)
+#' }
+"Germany"
+
+#####' 3 variables for Italy
+#'
+#' Se ha calculado el ratio de Goles y xG a favor y en contra para los equipos espanoles
+#'
+#' @format A data frame with 20 rows and 3 variables:
+#' \itemize{
+#'   \item Equipo: Team ID (character)
+#'   \item Favor: Goals/xG Goals Favor (double)
+#'   \item Contra: Goals/xG Goals Against (double)
+#' }
+"Italy"
+
+######' 3 variables for Teams
+#'
+#' Listado de los equipos incluidos en la libreria
+#'
+#' @format A data frame with 98 rows and 3 variables:
+#' \itemize{
+#'   \item Country: Country ID (character)
+#'   \item Team: Name (character)
+#'   \item Code: Code (character)
+#' }
+"Teams"
---
+++ R/escudos.R
@@ -0,0 +1,68 @@
+#' Esta funcion crea un grafico scatter plot con los escudos de las principales ligas europeas
+#'
+#' @param df Un df
+#' @param league Un literal que marca la liga que se quiere mostrar
+#' @return El grafico a partir de la tabla \code{df} con los escudos
+#' @examples
+#' PlotXY_Badges(df,"ESP")
+#' @export
+#'
+#' @import ggplot2
+#' @import ggimage
+#'
+PlotXY_Badges <- function(df,league_code){
+
+  names(df)<-c('Teams','Xaxis1','Yaxis1')
+
+  #contar numero de columnas (tiene que tener 3)
+  # la primera tiene que ser char y las otras numeric
+
+
+  #Identificamos la liga
+  if(league_code =="ESP"){
+    liga <- "ESP_"}
+  else if(league_code=="ENG"){
+    liga <- "ENG_"}
+  else if(league_code=="GER"){
+    liga <- "GER_"}
+  else if(league_code=="ITA"){
+    liga <- "ITA_"}
+  else if(league_code=="FRA"){
+    liga <- "FRA_"}
+  else{
+    return(c("Por favor seleccione el nombre correcto de la liga/Please select the correct name of the ligue: ESP, ENG, GER, ITA, FRA"))
+    }
+
+
+Tit <- readline("Titulo/Title: ")
+Stit <- readline("Subtitulo/Subtitle: ")
+xaxis <- readline("Etiqueta Eje X / Label x axis: ")
+yaxis <- readline("Etiqueta Eje Y / Label y axis: ")
+
+
+df$badge <- paste(system.file(package="FootballBadges"),"/Badges/",liga,df$Teams ,".png",sep="")
+
+
+p<-ggplot(data = df, aes(x = Xaxis1, y = Yaxis1)) +
+  ggtitle(label = Tit ,subtitle = Stit) +
+  geom_image(aes(image = badge), size = 0.07) +
+  geom_segment(data = df,aes(x = median(Yaxis1), y = 0, xend = median(Yaxis1), yend = max(Xaxis1)), linetype="dashed",colour = "gray61",alpha=0.5) +
+  geom_segment(data = df,aes(x = 0, y = median(Xaxis1), xend = max(Yaxis1), yend = median(Xaxis1)), linetype="dashed", colour = "gray61",alpha=0.5) +
+  xlab(xaxis) +
+  ylab(yaxis) +
+  annotate(geom="text", x=max(df$Yaxis1)-0.1, y=median(df$Xaxis1)+0.05, label=paste("Median =",format(round(median(df$Xaxis1), 2), nsmall = 2)),
+             color="gray",size=3) +
+  annotate(geom="text", x=median(df$Yaxis1)+0.1, y=max(df$Xaxis1), label=paste("Median =",format(round(median(df$Yaxis1), 2), nsmall = 2)),
+           color="gray",size=3) +
+  theme(legend.position="none") +
+  theme(plot.title = element_text(hjust = 0.5, face="bold.italic", size=14),
+        plot.subtitle = element_text(hjust = 0.5, face="bold.italic", size=10),
+        panel.background = element_blank(),
+        panel.border = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line = element_line(size = 0.5, linetype = "solid",
+                                 colour = "gray"))
+
+return(p)
+}
